846eab22d4c7463a9d7e84c93c0c8a36896501b0
volunteerocks <66223142+aabit@users.noreply.github.com>
2021-02-06 23:42:06 -0600

    Added temperature converter

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("After Conversion:");
+            Console.WriteLine("-----------------------------");
+            var celsius = TempConverter.FahrenheitToCelsius(86); 
+            Console.WriteLine($" {celsius} Celsius.");
+
+            
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(34);
+            Console.WriteLine($" {fahrenheit} Fahrenheit.");
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            var result = (f - 32) / 1.8;
+            return result;
+        }
+
+
+       public static double CelsiusToFahrenheit(double c)
+        {
+            var res = (c * 9) / 5 + 32;
+            return res;
+        } 
+}
+}
